cb1c602167c4144d71313bc5d2948ba8daebe778
Saloni Laddha <saloniladdha8@gmail.com>
2021-02-11 23:37:48 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,6 @@
+
 <!Doctype html>
+
 
 <html lang="en">
 
@@ -40,9 +42,9 @@
         <form class="form-signin" action="process_login.php" method="post" >
             <h2 class="form-signin-heading">Please sign in</h2>
              <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus name="email">
+                <input type="email" name="inputEmail" class="form-control" placeholder="Email address" >
              <label for="inputPassword" class="sr-only">Password</label>
-                 <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
+                 <input type="password" name="inputPassword" class="form-control" placeholder="Password" >
              <!-- <div class="checkbox">
                  <label><input type="checkbox" value="remember-me"> Remember me </label>
                          <a href="forget.php">/Forgot Password</a>
